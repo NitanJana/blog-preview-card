df83d73eea8eeda61dcb2c61fc03ae3e68d0cc42
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-02-10 22:58:27 +0530

    feat: add accessibility

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,16 +1,28 @@
 const Attribution = () => {
   return (
-    <div className="absolute bottom-2 text-center text-sm">
+    <footer className="absolute bottom-2 text-center text-sm">
       Challenge by{' '}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-custom-blue">
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="text-custom-blue"
+        tabIndex="0"
+        aria-label="Visit Frontend Mentor website"
+      >
         Frontend Mentor
       </a>
       . Coded by{' '}
-      <a href="https://github.com/NitanJana" target="_blank" className="text-custom-blue">
+      <a
+        href="https://github.com/NitanJana"
+        target="_blank"
+        className="text-custom-blue"
+        tabIndex="0"
+        aria-label="Visit Nitan Jana's GitHub profile"
+      >
         Nitan Jana
       </a>
       .
-    </div>
+    </footer>
   );
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,40 @@ import avatar from '../assets/images/image-avatar.webp';
 const Card = () => {
   return (
     <div className="bg-custom-black rounded-xl">
-      <div className="border-custom-black bg-custom-white group/card grid w-96 -translate-x-2 -translate-y-2 cursor-pointer items-center justify-center gap-4 rounded-xl border  p-5 transition duration-[400ms] hover:-translate-x-4 hover:-translate-y-4">
-        <img src={heroImg} alt="" className=" rounded-xl" />
-        <h3 className="bg-custom-yellow w-fit rounded px-3 py-1 font-extrabold">Learning</h3>
-        <p className="">Published 21 Dec 2023</p>
-        <h1 className="group-hover/card:text-custom-yellow  text-2xl font-extrabold">HTML & CSS foundations</h1>
-        <p className="text-custom-grey">
+      <main
+        role="main"
+        tabIndex={0}
+        className="border-custom-black bg-custom-white group/card grid w-96 -translate-x-2 -translate-y-2 cursor-pointer items-center justify-center gap-4 rounded-xl border  p-5 outline-none transition duration-[400ms] hover:-translate-x-4 hover:-translate-y-4 focus:-translate-x-4 focus:-translate-y-4"
+      >
+        <img tabIndex={0} src={heroImg} alt="" className=" rounded-xl" />
+        <header tabIndex={0} className="bg-custom-yellow w-fit rounded px-3 py-1 font-extrabold">
+          Learning
+        </header>
+        <p aria-label="Published December 21, 2023" tabIndex={0} className="">
+          Published 21 Dec 2023
+        </p>
+        <h1
+          tabIndex={0}
+          className="group-hover/card:text-custom-yellow group-focus/card:text-custom-yellow focus:text-custom-yellow text-2xl font-extrabold"
+        >
+          HTML & CSS foundations
+        </h1>
+        <p tabIndex={0} className="text-custom-grey">
           These languages are the backbone of every website, defining structure, content, and presentation.
         </p>
         <div className="flex items-center gap-4">
-          <img src={avatar} alt="avatar" className="w-10" />
-          <p className="font-bold">Greg Hooper</p>
+          <img
+            aria-label="Avatar of Greg Hooper"
+            tabIndex={0}
+            src={avatar}
+            alt="Avatar of Greg Hooper"
+            className="w-10"
+          />
+          <p tabIndex={0} className="font-bold">
+            Greg Hooper
+          </p>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
